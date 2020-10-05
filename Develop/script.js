22d6b9f086cb95006b3d61d22fa5ff09c5c95254
Edwin Mrtz <edwin.martinez.tech@gmail.com>
2020-10-04 17:29:49 -0700

    add functions that will make the random numbers

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,33 @@
 var generateBtn = document.querySelector("#generate");
 
 
+//generation of possible passwords
+// generate a random lower case letter
+var lowerLetter = function() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+//generate a random upper case letter
+var upperLetter = function() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+//generate a random number
+var numRandom = function() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+//generate a random sysmbols
+var symRandom = function() {
+    var symbol = "!@#$%^&*()[]{}=<>/:";
+    return symbol[Math.floor(Math.random() * symbol.length)];
+}
+
+console.log(upperLetter());
+console.log(lowerLetter());
+console.log(symRandom());
+console.log(numRandom());
+
 
 // Write password to the #password input
 function writePassword() {
